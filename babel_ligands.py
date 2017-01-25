58ecf3660c43a053fb508030401e653baf7fced6
lmturner20 <LMT72@pitt.edu>
2017-01-25 15:58:00 -0500

    Update babel_ligands.py

--- babel_ligands.py
+++ babel_ligands.py
@@ -7,4 +7,4 @@ strings = [ name[0:3] for name in strings]
 for item in strings:
     smiItem = item + "_lig.smi"
     sdfItem = item + "_lig.sdf"
-    babel --gen3D ~/item/smiItem ~/item/sdfItem
+    babel --gen3D /home/dkoes/PDBbind/general-set-except-refined/item/smiItem /home/dkoes/PDBbind/general-set-except-refined/item/sdfItem
