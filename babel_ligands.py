7035467881bb700155f8dad84fbfc2897fe77ae1
lmturner20 <LMT72@pitt.edu>
2017-01-25 20:01:34 -0500

    Update babel_ligands.py

--- babel_ligands.py
+++ babel_ligands.py
@@ -4,8 +4,8 @@ string = infile.readline()
     
 while string !=' ':
 	string = string[0:3]
-	smiItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.smi"
-	sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.sdf"
+	smiItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_lig.smi"
+	sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_lig.sdf"
 	cmd1 = "babel --gen3D smiItem sdfItem"
 	os.system(cmd1)
 	string = infile.readLine()
