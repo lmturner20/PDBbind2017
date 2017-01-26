c823caa9035f0aeba32a6b31c1d33dac152a89dd
lmturner20 <LMT72@pitt.edu>
2017-01-25 19:58:25 -0500

    Update babel_ligands.py

--- babel_ligands.py
+++ babel_ligands.py
@@ -1,12 +1,13 @@
 import os
-with open('Problem_Ligands') as infile:
-	strings = infile.readLines()
+infile = open('Problem_Ligands;, 'r')
+string = infile.readLine()
+    
+while line !=' ':
+	string = string[0:3]
+	smiItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.smi"
+	sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.sdf"
+	cmd1 = "babel --gen3D smiItem sdfItem"
+	os.system(cmd1)
+	string = infile.readLine()
+
 infile.close()
-
-strings = [ name[0:3] for name in strings]
-
-for item in strings:
-    smiItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.smi"
-    sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+item + "/" + item + "_lig.sdf"
-    cmd1 = "babel --gen3D smiItem sdfItem"
-    os.system(cmd1)
