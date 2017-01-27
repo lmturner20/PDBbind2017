e07d1d30b9a3d687297fc640c3a2e69746423a3c
lmturner20 <LMT72@pitt.edu>
2017-01-26 19:48:54 -0500

    Create dockTheLigands.py

---
+++ dockTheLigands.py
@@ -0,0 +1,15 @@
+import os
+infile = open('Problem_Ligands', 'r')
+string = infile.readline()
+    
+while string !=' ':
+	string = string[0:4]
+	pdbItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_rec.pdb"
+	sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_lig.sdf"
+  pocketItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_pocket.pdb"
+  dockedItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_docked.sdf.gz"
+	cmd1 = ("smina -r " + pdbItem + " -l " + sdfItem + " --autobox_ligand " + pocketItem + " --num_modes 25 --seed 0 -o " + dockedItem + " --cpu 8 --exhaustiveness 50")
+	os.system(cmd1)
+	string = infile.readline()
+
+infile.close()
