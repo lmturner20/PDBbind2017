0f3fe2b4f44cf68fba399370c329d38d4543a05f
lmturner20 <LMT72@pitt.edu>
2017-01-26 19:51:18 -0500

    Update dockTheLigands.py

--- dockTheLigands.py
+++ dockTheLigands.py
@@ -6,8 +6,8 @@ while string !=' ':
 	string = string[0:4]
 	pdbItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_rec.pdb"
 	sdfItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_lig.sdf"
-  pocketItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_pocket.pdb"
-  dockedItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_docked.sdf.gz"
+	pocketItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_pocket.pdb"
+	dockedItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_docked.sdf.gz"
 	cmd1 = ("smina -r " + pdbItem + " -l " + sdfItem + " --autobox_ligand " + pocketItem + " --num_modes 25 --seed 0 -o " + dockedItem + " --cpu 8 --exhaustiveness 50")
 	os.system(cmd1)
 	string = infile.readline()
