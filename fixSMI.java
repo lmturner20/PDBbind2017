2e597ac220d5fc9fd45253f5f56bcdcc7d9464b8
lmturner20 <LMT72@pitt.edu>
2017-02-03 20:28:13 -0500

    Update fixSMI.java

--- fixSMI.java
+++ fixSMI.java
@@ -2,6 +2,6 @@ public class fixSMI
 {
 	public static void main( String[] args )
 	{
-		System.out.print( "[N]=12[Ir+3]3456([Cl-])([C@-]7([C]3(C)=[C]4(C)[C]5(C)=[C]67C)C)[N-](S(=O)(=O)c3ccccc3)CC=1C=C(c1ccc(cc1)S(=O)(=O)N)C=C2	3zp9_ligand" );
+		System.out.print( "COc1cccc(OC)c1OC(=O)C1(C=CC=C1)[Ru@]1([C-]#[O+])c2cc3nc(C)nc(N)c3cc2C2=CC=CC=[N]12	3whw_ligand" );
 	}
 }
