7d6b404b554274be3fb6df226e7789f4576018c8
lmturner20 <LMT72@pitt.edu>
2017-02-09 16:03:32 -0500

    Update runGnina2.py

--- runGnina2.py
+++ runGnina2.py
@@ -5,7 +5,7 @@ string = infile.readline()
 while string !="":
 	string = string[0:4]
 	pdbItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_rec.pdb"
-  gninaItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_rec.gninatypes"
+ 	gninaItem = "/home/dkoes/PDBbind/general-set-except-refined/"+string + "/" + string + "_rec.gninatypes"
 	cmd1 = ("gninatyper " + pdbItem + " " + gninaItem)
 	os.system(cmd1)
 	print(string)
