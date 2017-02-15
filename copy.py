9b54398ccc66aeaac6605619aaa74b7f8b572c09
lmturner20 <LMT72@pitt.edu>
2017-02-15 10:07:19 -0500

    Update copy.py

--- copy.py
+++ copy.py
@@ -6,7 +6,7 @@ while string !=' ':
 	string = string[0:4]
 	dir1 = "/home/dkoes/PDBbind/general-set-except-refined/" + string
 	dir2 = "/home/dkoes/PDBbind/general-set-with-refined/" + string
-	cmd1 = ("cp -r " + dir1 + dir2)
+	cmd1 = ("cp -R " + dir1 + dir2)
 	os.system(cmd1)
 	string = infile.readline()
 
