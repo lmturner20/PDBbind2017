dc8bf1e56672bdf5ca1725793a9bbc58d9058a56
lmturner20 <LMT72@pitt.edu>
2017-02-15 09:55:27 -0500

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,13 @@
+import os
+infile = open('dock180.txt', 'r')
+string = infile.readline()
+    
+while string !=' ':
+	string = string[0:4]
+	dir1 = "/home/dkoes/PDBbind/general-set-except-refined/" + string
+	dir2 = "/home/dkoes/PDBbind/general-set-with-refined/" + string
+	cmd1 = ("cp -r " + dir1 + dir2)
+	os.system(cmd1)
+	string = infile.readline()
+
+infile.close()
