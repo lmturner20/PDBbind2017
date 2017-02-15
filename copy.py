a6bb2a8e04e10d359ec23aec2f41f656ca0b5380
lmturner20 <LMT72@pitt.edu>
2017-02-15 12:36:44 -0500

    Update copy.py

--- copy.py
+++ copy.py
@@ -3,8 +3,8 @@ infile = open('FinalRefSet', 'r')
     
 for string in infile:
 	string = string[0:4]
-	dir1 = "/home/dkoes/PDBbind/general-set-except-refined/" + string
-	dir2 = "/home/dkoes/PDBbind/refined-set/" + string
+	dir1 = "/home/dkoes/PDBbind/refined-set/" + string
+	dir2 = "/home/dkoes/PDBbind/general-set-with-refined/" + string
 	cmd1 = ("cp -R " + dir1 + " " + dir2)
 	os.system(cmd1)
 
