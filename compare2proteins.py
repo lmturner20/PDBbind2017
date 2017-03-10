50ecb1dcc5c4723c9b4b4241b21b91279f8c41da
lmturner20 <LMT72@pitt.edu>
2017-03-09 21:22:51 -0500

    Update compare2proteins.py

--- compare2proteins.py
+++ compare2proteins.py
@@ -1,6 +1,7 @@
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Polypeptide import is_aa
 from Bio.PDB.Polypeptide import three_to_one
+from Bio import pairwise2
 
 def getResidueString(structure):
     seq=''
