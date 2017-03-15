ca8d810f222a87af5b4d311bcc378102642d080c
lmturner20 <LMT72@pitt.edu>
2017-03-15 15:53:52 -0400

    Update compute_row.py

--- compute_row.py
+++ compute_row.py
@@ -22,9 +22,10 @@ def getResidueString(structure):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
+    parser.add_argument('-i','--input',type=str,required=True)
     p= PDBParser(PERMISSIVE=1,QUIET=1)
 
-    name = args
+    name = args.input
     handle = ("/home/dkoes/PDBbind/general-set-with-refined/%s/%s_rec.pdb" %(name, name))
     structure = p.get_structure(name, handle)
     seq=getResidueString(structure)
