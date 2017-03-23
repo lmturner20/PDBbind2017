699d9db953637775cb1b15480dd32ca0625859e6
lmturner20 <LMT72@pitt.edu>
2017-03-22 21:25:17 -0400

    Update heatMap.py

--- heatMap.py
+++ heatMap.py
@@ -11,7 +11,7 @@ index = dict()
 for (i,fname) in enumerate(glob.glob('/home/lmt72/PDBdistances/*.distances')):
     fname = os.path.basename(fname)
     if fname.endswith('.distances'):
-        pdb = fname#[:-10]
+        pdb = fname[:-10]
         index[pdb] = i
 print pdb
 length = len(index)
