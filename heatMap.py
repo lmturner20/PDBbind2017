74df0fa8e82b3a19c397857a8b6ee24be9f4a57d
lmturner20 <LMT72@pitt.edu>
2017-03-22 20:13:30 -0400

    Update heatMap.py

--- heatMap.py
+++ heatMap.py
@@ -8,7 +8,7 @@ import glob
 
 #a = [12482][12842]
 dict = {}
-for (i,fname) in enumerate(glob.glob('/home/dkoes/PDBbind/general-set-with-refined/*/*.distances')):
+for (i,fname) in enumerate(glob.glob('/home/dkoes/PDBdistances/*.distances')):
     pdb = fname.rstrip('.distances')
     dict[pdb] = i
 length = len(dict)
