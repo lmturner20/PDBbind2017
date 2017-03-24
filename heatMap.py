aee78365310917cf84ab8df33db5cf0847fe6bbe
lmturner20 <LMT72@pitt.edu>
2017-03-23 22:08:36 -0400

    Update heatMap.py

--- heatMap.py
+++ heatMap.py
@@ -41,21 +41,17 @@ D = D[:,l2]
 cPickle.dump((npArray,D, Z1, names),open("clusterstate.pickle",'w'),-1)
 
 (npArray, D, Z1, names) = cPickle.load(open("clusterstate.pickle"))
-fig = plt.figure(figsize=(8,8))
 ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
 ax1.set_xticks([])
 ax1.set_yticks([])
 
 # Compute and plot second dendrogram.
-ax2 = fig.add_axes([0.3,0.71,0.6,0.15])
 ax2.set_xticks([])
 ax2.set_yticks([])
 
 # Plot distance matrix.
-axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
 im = PIL.Image.fromarray(cm.gist_earth(D,bytes=True))
 
 fig.subplots_adjust(bottom=0.1)
 axcolor = fig.add_axes([0.3,0.07,0.6,0.02])
-fig.colorbar(im,cax=axcolor,orientation='horizontal')
 im.savefig('heatmap',dpi=600)
