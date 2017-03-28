11afc51ca88f7c3f72adf9819d604503957cddd1
lmturner20 <LMT72@pitt.edu>
2017-03-27 23:33:31 -0400

    Update compute_row2.py

--- compute_row2.py
+++ compute_row2.py
@@ -29,17 +29,22 @@ if __name__ == '__main__':
     handle = ("/net/pulsar/home/koes/dkoes/PDBbind/general-set-with-refined/%s/%s_rec.pdb" %(name, name))
     structure = p.get_structure(name, handle)
     chains = [ch for ch in structure.get_chains()]
+    print "First chains done"
 
     for secondHandle in glob.glob('/net/pulsar/home/koes/dkoes/PDBbind/general-set-with-refined/*/*_rec.pdb'):
         data= secondHandle.split("/")
         secondName = data[-2]
+        print "Second info loaded"
         secondStructure = p.get_structure(secondName, secondHandle)
         secondChains = [ch for ch in secondStructure.get_chains()]
+        print "Second chains done"
         
     for chain in chains:
         seq=getResidueString(chain)
+        print "First seq calculated"
         for twoChain in secondChains:
             secondSeq=getResidueString(twoChain)
+            print "Second seq calculated"
             score = pairwise2.align.globalxx(seq, secondSeq, score_only=True)
             length= min(len(seq), len(secondSeq))
             distance = (length-score)/length
