5554a2f67b2880fe34ae1571acd5580eead60379
lmturner20 <LMT72@pitt.edu>
2017-04-04 10:58:57 -0400

    Update plotHeatMap.py

--- plotHeatMap.py
+++ plotHeatMap.py
@@ -11,5 +11,5 @@ import cPickle
 import PIL
 
 (npArray, D, Z1, names) = cPickle.load(open("clusterstate.pickle"))
-im = PIL.Image.fromarray(cm.Vega20b(D,bytes=True))
+im = PIL.Image.fromarray(cm.seismic(D,bytes=True))
 im.save('heatmap.png')
